b0dc99db01088d79dc750454dd22c21a90ed5793
kushal tirumala <kushaltirumala99@gmail.com>
2015-07-14 00:38:54 -0700

    playing around with more stuff- still not working

---
+++ js/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// angular.js main app initialization
+var app = angular.module('example359', []).
+    config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.
+        when('/', { templateUrl: 'pages/index.html', activetab: 'projects', controller: HomeCtrl }).
+        when('/project/:projectId', {
+          templateUrl: function (params) { return 'pages/' + params.projectId + '.html'; },
+          controller: ProjectCtrl,
+          activetab: 'projects'
+        }).
+        when('/privacy', {
+          templateUrl: 'pages/privacy.html',
+          controller: PrivacyCtrl,
+          activetab: 'privacy'
+        }).
+        when('/about', {
+          templateUrl: 'pages/about.html',
+          controller: AboutCtrl,
+          activetab: 'about'
+        }).
+        otherwise({ redirectTo: '/' });
+    }]).run(['$rootScope', '$http', '$browser', '$timeout', "$route", function ($scope, $http, $browser, $timeout, $route) {
+
+        $scope.$on("$routeChangeSuccess", function (scope, next, current) {
+          $scope.part = $route.current.activetab;
+        });
+
+        // onclick event handlers
+        $scope.showForm = function () {
+          $('.contactRow').slideToggle();
+        };
+        $scope.closeForm = function () {
+          $('.contactRow').slideUp();
+        };
+
+        // save the 'Contact Us' form
+        $scope.save = function () {
+          $scope.loaded = true;
+          $scope.process = true;
+          $http.post('sendemail.php', $scope.message).success(function () {
+              $scope.success = true;
+              $scope.process = false;
+          });
+        };
+  }]);
+
+app.config(['$locationProvider', function($location) {
+    $location.hashPrefix('!');
+}]);
---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// optional controllers
+function HomeCtrl($scope, $http) {
+}
+
+function ProjectCtrl($scope, $http) {
+}
+
+function PrivacyCtrl($scope, $http, $timeout) {
+}
+
+function AboutCtrl($scope, $http, $timeout) {
+}
