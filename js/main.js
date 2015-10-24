36db253af74779b957a96c325dd1fe1b0cf888dc
campbellerickson <campbellerickson1@gmail.com>
2015-10-24 07:57:33 -0700

    Yeet

--- js/main.js
+++ js/main.js
@@ -111,3 +111,37 @@ jQuery(function($) {'use strict';
 	}
 
 });
+
+
+var options = {
+    binder_id: BINDER_ID,
+    iframe: true,
+    tagid4iframe: "container",
+    iframewidth: "920px",
+    iframeheight: "650px",
+    autostart_meet: true,
+    autostart_note: false,
+    start_chat: function(event) {
+        alert("Chat started session Id: " + event.session_id);
+    },
+    start_meet: function(event) {
+        alert("Meet started session key: " + event.session_key + " session id: " + event.session_id);
+    },
+    end_meet: function(event) {
+        alert("Meet end event");
+    },
+    invite_member: function(event) {
+        alert("Invite member into binder Id: " + event.binder_id);
+    },
+    request_note: function(event) {
+        alert("Note start request");
+    },
+    error: function(event) {
+        alert("Chat error code: " + event.error_code + " error message: " + event.error_message);
+    }
+};
+Moxtra.chat(options);
+
+
+
+
